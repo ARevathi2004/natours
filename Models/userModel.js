35f8aab7b92f37144da240c0267bd0e0e607ca9d
Revathi <revathi.a084@gmail.com>
2025-06-03 19:34:30 +0530

    updated Model/userModel.js

--- Models/userModel.js
+++ Models/userModel.js
@@ -56,25 +56,25 @@ const userSchema=new mongoose.Schema({
         select:false
     }
 });
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  this.passwordConfirm = undefined;
-  next();
-});
-
-
-// userSchema.pre('save',async function(next){
-//     // Only runs this function if password was actually modified
-//     if(!this.isModified('password')) return next();
-
-//     // Hash the password with cost of 12
-//     this.password= await bcrypt.hash(this.password,12);
-
-//     // Delete the passwordconfirm field
-//     this.passwordConfirm=undefined;
-//     next();
+// userSchema.pre('save', async function (next) {
+//   if (!this.isModified('password')) return next();
+//   this.password = await bcrypt.hash(this.password, 12);
+//   this.passwordConfirm = undefined;
+//   next();
 // });
+
+
+userSchema.pre('save',async function(next){
+    // Only runs this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+    // Hash the password with cost of 12
+    this.password= await bcrypt.hash(this.password,12);
+
+    // Delete the passwordconfirm field
+    this.passwordConfirm=undefined;
+    next();
+});
 
 userSchema.pre('save',function(next){
 if(!this.isModified('password' || this.isNew)) return next();
@@ -88,6 +88,8 @@ userSchema.pre(/^find/,function(next){
     this.find({active:{$ne:false}});
     next();
 });
+
+
 
 userSchema.methods.correctPassword=async function(candidatePassword,userPassword){
     return await bcrypt.compare(candidatePassword,userPassword);

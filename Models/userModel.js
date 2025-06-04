34b62330b542dff1a0a8ac5b27260ed1f299c6d9
Revathi <revathi.a084@gmail.com>
2025-06-04 11:47:00 +0530

    updated Models/userModel.js

--- Models/userModel.js
+++ Models/userModel.js
@@ -69,7 +69,11 @@ userSchema.pre('save',async function(next){
     if(!this.isModified('password')) return next();
 
     // Hash the password with cost of 12
-    this.password= await bcrypt.hash(this.password,12);
+    if (!this.password.startsWith('$2a$') && !this.password.startsWith('$2b$')) {
+  this.password = await bcrypt.hash(this.password, 12);
+}
+
+    //this.password= await bcrypt.hash(this.password,12);
 
     // Delete the passwordconfirm field
     this.passwordConfirm=undefined;

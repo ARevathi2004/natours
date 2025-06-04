37ad61238da9dd935c78d4aba13282dce1b76f22
Revathi <revathi.a084@gmail.com>
2025-06-04 12:09:04 +0530

    updated controller/authController.js

--- controller/authController.js
+++ controller/authController.js
@@ -13,15 +13,20 @@ const signToken=id=>{
     });
 };
 
-const createSendToken=(user,statuscode,res)=>{
+const createSendToken=(user,statuscode,req,res)=>{
     const token=signToken(user._id);
-    const cookieOptions={
-        expires:new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60* 60 * 1000),
-        httpOnly:true 
-       };
-       if(process.env.NODE_ENV==='production') cookieOptions.secure=true;
+    // const cookieOptions={
+    //     expires:new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60* 60 * 1000),
+    //     httpOnly:true,
+    //     secure:req.secure || req.headers('x-forwarded-proto') === 'https' 
+    //    };
+       // if(process.env.NODE_ENV==='production') cookieOptions.secure=true;
 
-    res.cookie('jwt',token,cookieOptions);
+    res.cookie('jwt',token,{
+        expires:new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60* 60 * 1000),
+        httpOnly:true,
+        secure:req.secure || req.headers('x-forwarded-proto') === 'https' 
+       });
     // remove the password
     user.password=undefined;
     res.status(statuscode).json({
@@ -44,7 +49,7 @@ exports.signup=catchAsync(async (req,res,next) => {
     const url=`${req.protocol}://${req.get('host')}/me`;
     //console.log(url);
     await new Email(newUser,url).sendWelcome();
-    createSendToken(newUser,201,res);
+    createSendToken(newUser,201,req,res);
     
 });
 
@@ -65,7 +70,7 @@ if(!user || !(await user.correctPassword(password,user.password))){
 
 // 3) If everything ok ,send token to client
 
-createSendToken(user,200,res);
+createSendToken(user,200,req,res);
 });
 
 exports.logout=(req,res)=>{
@@ -204,7 +209,7 @@ exports.resetPassword=catchAsync(async(req,res,next)=>{
     // 3) Update changePasswordAt property for the user
 
     // 4) Log the user in,send JWT 
-    createSendToken(user,200,res);
+    createSendToken(user,200,req,res);
 });
 
 exports.updatePassword= catchAsync(async(req,res,next)=>{
@@ -222,7 +227,7 @@ exports.updatePassword= catchAsync(async(req,res,next)=>{
     // User.findByIdandUpdate will NOT work as indended
 
     // 4) Log user in , send JWT
-    createSendToken(user,200,res);
+    createSendToken(user,200,req,res);
    
 
 });

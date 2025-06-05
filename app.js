4cba6fc6eb7dcbc909d7b07eae23107e91ec162c
Revathi <revathi.a084@gmail.com>
2025-06-05 14:17:32 +0530

    improved stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter=require('./routes/reviewRoutes');
 const bookingRouter=require('./routes/bookingRoutes');
+const bookingController=require('./controller/bookingController');
 const viewRouter=require('./routes/viewRoutes');
 
 // Start express app
@@ -84,6 +85,11 @@ const limiter=rateLimit({
   message:'Too many request to this IP,Please try again in hour'
 });
 app.use('/api',limiter);
+
+app.post('/webhook-checkout',
+  express.raw({type:'application/json'}),
+  bookingController.webhookCheckout
+);
 
 // Body parser ,reading data from body into req.body
 app.use(express.json({limit:'10kb'}));
